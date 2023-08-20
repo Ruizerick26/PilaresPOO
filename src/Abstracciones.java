fdad34217fb7acca4ae63947766bab1b529b43f0
Ruizerick26 <ruizerick2525@gmail.com>
2023-08-20 18:27:52 -0500

    Ejemplo Abstracciones

---
+++ src/Abstracciones.java
@@ -0,0 +1,48 @@
+public class Abstracciones {
+    //Crearemos los atributos de un auto en esta clase llamada abstracciones
+    private String modelo;
+    private int anio;
+    private String marca;
+    private int cilindraje;
+    private float kilometraje;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getCilindraje() {
+        return cilindraje;
+    }
+
+    public void setCilindraje(int cilindraje) {
+        this.cilindraje = cilindraje;
+    }
+
+    public float getKilometraje() {
+        return kilometraje;
+    }
+
+    public void setKilometraje(float kilometraje) {
+        this.kilometraje = kilometraje;
+    }
+}
